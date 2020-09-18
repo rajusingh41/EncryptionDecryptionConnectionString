4a50294865305042bf7390fc1869a6172d987bdc
rajusingh41 <raju.singh41@gmail.com>
2020-09-18 15:28:09 +0530

    Add project files.

---
+++ EncryptionDecryptionConnectionString/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using ConnectionStringCryptographyHelper;
+using System;
+using System.Windows;
+
+namespace EncryptionDecryptionConnectionString
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Encrypt_Click(object sender, RoutedEventArgs e)
+        {
+            var planText = txt_PlanText.Text;
+            if(!string.IsNullOrEmpty(planText))
+            {
+                try
+                {
+                    var encryptedText = CryptographyHelper.Encrypt(planText);
+                    txt_EncryptedText.Text = encryptedText;
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please Enter data which you want to Encrypt!");
+            }
+           
+        }
+
+        private void btn_Decrypt_Click(object sender, RoutedEventArgs e)
+        {
+            var encryptedText = txt_EncryptedText.Text;
+            if (!string.IsNullOrEmpty(encryptedText))
+            {
+                try
+                {
+                    var decryptText = CryptographyHelper.Decrypt(encryptedText);
+                   txt_Decrypted.Text = decryptText;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please Enter data which you want to Encrypt!");
+            }
+        }
+    }
+}
